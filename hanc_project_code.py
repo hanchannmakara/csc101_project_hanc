55f88e427fd473a8eff03b3ab4d33a82b539c066
hanc <hanmakara6@gmail.com>
2017-12-11 10:30:06 -0500

    Making translation

--- hanc_project_code.py
+++ hanc_project_code.py
@@ -3,14 +3,18 @@ from dictionary import*
 class HancSecurity:
     #This part will be for encryption and decryption purpose
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
     def __init__(self, input_file = "Input File", key = 0, crypt_type = "encrypt"):
         self.input_file = input_file
-        self.key = key
+        self.key= key
         self.crypt_type = crypt_type
         self.cipher = ""
         self.message = ""
-        #self.import_file()
+        self.import_file()
+
+        self.t1 = Dictionary(self.key, self.input_file).dict1
+        self.t2 = Dictionary(self.key, self.input_file).dict2
+        self.t3 = Dictionary(self.key, self.input_file).key_functions()[0]
+        self.t4 = Dictionary(self.key, self.input_file).key_functions()[1]
 
     def import_file(self):
         f = open(self.input_file, "r")
@@ -28,45 +32,41 @@ class HancSecurity:
 
     def encrypt(self):
         output = ""
-        k = Dictionary(self.key, self.input_file)
         for i in self.message:
             if i.upper() in self.alphabet:
-                v = k.dict1[i.upper()]
-                u = k.key_functions()[0][v]
+                v = self.t1[i.upper()]
+                u = self.t3[v]
                 output += u
             else:
                 output += i
-        print(output)
         return output
 
     def decrypt(self):
-        decrypt_list = []
-        k = Dictionary(self.key, self.input_file)
-        for i in self.cipher:
-            if i in self.alphabet:
-                m = k.key_functions()[1][i]
-                decrypt_list.append(m)
-            else:
-                decrypt_list.append(i)
         output = ""
-        for i in range(len(decrypt_list)):
-            if type(decrypt_list[i]) is int:
-                output += k.dict2[decrypt_list[i]]
+        convert_list = []
+        for i in self.cipher():
+            if i in self.alphabet:
+                m = self.t4[i]
+                convert_list.append(m)
             else:
-                output += str(decrypt_list[i])
-        print(output)
+                convert_list.append(i)
+        for i in range(len(convert_list)):
+            if type(convert_list[i]) is int:
+                output += self.t2[i]
+            else:
+                output += str(convert_list[i])
         return output
-
 
 def main():
     #A sample encryption
-    cipher0 = HancSecurity("message_input.txt", 5, "encrypt")
+    cipher0 = HancSecurity("message_input.txt", 3, "encrypt")
     cipher_text0 = cipher0.encrypt()
     cipher0.export_file(cipher_text0, "hanc_smp1.txt")
 
-    #cipher3 = HancSecurity(, 5, "decrypt")
-    #cipher_text3 = cipher3.decrypt()
-    #cipher3.export_file(cipher_text3, "hanc_samp1_decrypt.txt")
+    cipher3 = HancSecurity("hanc_smp1.txt", 3, "decrypt")
+    cipher_text3 = cipher3.decrypt()
+    cipher3.export_file(cipher_text3, "hanc_samp1_decrypt.txt")
+
 
 
 
